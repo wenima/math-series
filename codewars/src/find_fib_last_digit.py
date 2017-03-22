68997784855aa39bf13e12d37b7c93edf55e21ac
Marc Kessler <marc.kesslernyc@gmail.com>
2017-03-22 13:30:53 -0700

    changed string cast to taking modulo to get last digit

--- codewars/src/find_fib_last_digit.py
+++ codewars/src/find_fib_last_digit.py
@@ -1,10 +1,7 @@
 """This module solves kata https://www.codewars.com/kata/find-fibonacci-last-digit."""
 
-def iter_fib(n):
+def get_last_digit(n):
     a, b = 0, 1
     for i in range(n-1):
-        a, b = b, a+b
+        a, b = b, (a+b) % 10
     return b
-
-def get_last_digit(n):
-    return int(str(iter_fib(n))[-1])
