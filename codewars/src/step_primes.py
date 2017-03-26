7bf798d45aef95a09a02a0260b19aa39b9c13024
Marc Kessler <marc.kesslernyc@gmail.com>
2017-03-26 14:27:34 -0700

    add test for larger prime values which showed the algo which passed basic test was not efficient enough. Rewrote the algo to generate and test primes as needed, not generate full list

--- codewars/src/step_primes.py
+++ codewars/src/step_primes.py
@@ -2,7 +2,6 @@
 
 from itertools import islice
 
-
 def prime(a):
     """Return True if a is prime."""
     if a == 2: return True
@@ -10,10 +9,18 @@ def prime(a):
     return not any(a % x == 0 for x in range(3, int(a**0.5) + 1, 2))
 
 
+def find_next_prime(m, n):
+    for i in range(m, n, 2):
+        if prime(i):
+            yield i
+
+
 def step(g, m, n):
-    """Return the first pair of primes between m and n with step g."""
-    primes = [i for i in range(m, n) if prime(i)]
-    try:
-        return next([p, q] for idx, p in enumerate(primes, 1) for q in islice(primes, idx, len(primes)) if q - p == g)
-    except StopIteration:
-        return None
+    """Return the first pair of primes between m and n with step g by recursively
+    finding the next prime and check if that prime + steps is a prime and thus a match."""
+    if m % 2 == 0: m += 1   #since we want to step over all even numbers, need to seed an odd number
+    if prime(m) and prime(m + g):
+        return [m, m + g]
+    while m + g < n:
+        m = next(find_next_prime(m + 2, n)) #gets the next prime from generator function
+        return [m, m + g] if prime(m + g) else step(g, m, n) #call step recursively with next prime
