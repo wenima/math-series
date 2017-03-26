c0bdb6445287cf2344957532e2df596ab89319c6
Marc Kessler <marc.kesslernyc@gmail.com>
2017-03-26 15:50:43 -0700

    gave up recursion in favor of a better iterative solution

--- codewars/src/step_primes.py
+++ codewars/src/step_primes.py
@@ -1,7 +1,5 @@
 """Module to solve the code-kata https://www.codewars.com/kata/steps-in-primes/python."""
 
-from itertools import islice
-
 def prime(a):
     """Return True if a is prime."""
     if a == 2: return True
@@ -9,18 +7,10 @@ def prime(a):
     return not any(a % x == 0 for x in range(3, int(a**0.5) + 1, 2))
 
 
-def find_next_prime(m, n):
-    for i in range(m, n, 2):
-        if prime(i):
-            yield i
-
-
 def step(g, m, n):
-    """Return the first pair of primes between m and n with step g by recursively
-    finding the next prime and check if that prime + steps is a prime and thus a match."""
-    if m % 2 == 0: m += 1   #since we want to step over all even numbers, need to seed an odd number
-    if prime(m) and prime(m + g):
-        return [m, m + g]
-    while m + g < n:
-        m = next(find_next_prime(m + 2, n)) #gets the next prime from generator function
-        return [m, m + g] if prime(m + g) else step(g, m, n) #call step recursively with next prime
+    """Return the first pair of primes between m and n with step g."""
+    if m % 2 == 0: m += 1 #need to see an odd number so we can step over known composites
+    try:
+        return next([x, x + g] for x in range(m, n - g, 2) if prime(x) and prime(x + g))
+    except StopIteration:
+        return None
