e279f612615436832e858db3f3657143314c4f91
MarcMBPDev <marc.wenicker@gmail.com>
2017-04-27 23:24:18 -0700

    first pass of base test

--- codewars/src/k_primes.py
+++ codewars/src/k_primes.py
@@ -1,7 +1,43 @@
 """Module to solve https://www.codewars.com/kata/k-primes/python."""
 
 
+def eratosthenes_step2(n):
+    """Return all primes up to and including sqrt of n.
+   Since we know primes can't be even, we iterate in steps of 2."""
+    if n >= 2:
+        yield 2
+    multiples = set()
+    for i in range(3, n+1, 2):
+        if i not in multiples:
+            yield i
+            multiples.update(range(i*i, n+1, i))
+
+
+def prime_factors(n, primes, k):
+    """Return a list of prime factors for a given number in ascending order if
+    the number of prime factors equals k."""
+    factors = []
+    if n < 2:
+        return factors
+    for p in primes:
+        while True:
+            if n % p:
+                break
+            else:
+                n = n // p
+                factors.append(p)
+                if n < p:
+                    return factors if len(factors) == k else None
+                if len(factors) > k:
+                    return None
+    return factors if len(factors) == k else None
+
+
 def countKprimes(k, start, end):
     """Return a list of k-primes between start and end. A k-prime is a number which
     has exactly k primes, multiplicity counted."""
-    pass
+    primes = [p for p in eratosthenes_step2(end//2 ** (k - 1))] #only primes up to 2**(k-1) are possible prime factors
+    out = []
+    for n in range(start, end + 1):
+        if prime_factors(n, primes, k): out.append(n)
+    return out
