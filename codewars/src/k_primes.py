222f52b7c925ea6641a54f3634369620b05dc171
MarcMBPDev <marc.wenicker@gmail.com>
2017-04-28 13:58:45 -0700

    add test cases, refactor prime generation, deleted code is debugged code

--- codewars/src/k_primes.py
+++ codewars/src/k_primes.py
@@ -17,8 +17,6 @@ def prime_factors(n, primes, k):
     """Return a list of prime factors for a given number in ascending order if
     the number of prime factors equals k."""
     factors = []
-    if n < 2:
-        return factors
     for p in primes:
         while True:
             if n % p:
@@ -33,10 +31,23 @@ def prime_factors(n, primes, k):
     return factors if len(factors) == k else None
 
 
+def get_primes(n):
+    """Return a list of primes up to and including n."""
+    return [p for p in eratosthenes_step2(n)]
+
+
 def countKprimes(k, start, end):
-    """Return a list of k-primes between start and end. A k-prime is a number which
-    has exactly k primes, multiplicity counted."""
-    primes = [p for p in eratosthenes_step2(end//2 ** (k - 1))] #only primes up to 2**(k-1) are possible prime factors
+    """Return a list of k-primes betwene start and end. A k-prime is a number
+    which has exactly k primes, multiplicity counted. Before generating primes,
+    we cap the range of primes to only those needed."""
+    if end//2 ** (k -1) > 0:
+        primes = get_primes(end//2 ** (k - 1))#only primes up to 2**(k-1) are possible prime factors
+        return find_k_primes(k, start, end, primes)
+    return []
+
+
+def find_k_primes(k, start, end, primes=None):
+    """Return a list of k-primes between start and end given a list of primes."""
     out = []
     for n in range(start, end + 1):
         if prime_factors(n, primes, k): out.append(n)
