ce48514e16a811c6cf16ab0f0467dfd3512c1872
MarcMBPDev <marc.wenicker@gmail.com>
2017-04-29 16:46:57 -0700

    pass test for prime factorization

--- codewars/src/sum_first_last_pf.py
+++ codewars/src/sum_first_last_pf.py
@@ -2,9 +2,32 @@
 
 def sflpf_data(val, nMax):
     """Return a sorted list of numbers whose first and last prime factor sum up to val."""
-    pass
+    out = []
+    for n in range(4, end + 1):
+        if n % 2:
+            if is_prime(n):
+                continue
+        pf = prime_factors(n)
+        if pf[0] + pf [-1] == val: out.append(n)
+    return out
+
+
+def is_prime(n):
+    """Return True if n is prime."""
+    if (n % 2 == 0 and n > 2) or n == 1:
+        return False
+    return all(n % i for i in range(3, int(n**0.5) + 1, 2))
 
 
 def prime_factors(n):
     """Return a list of prime factors for n."""
-    pass
+    factors = []
+    p = 2
+    while n >= (p * p):
+        if n % p:
+            p += 1
+        else:
+            n = n // p
+            factors.append(p)
+    factors.append(n)
+    return factors
