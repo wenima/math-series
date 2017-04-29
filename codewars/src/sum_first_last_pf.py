68f948451ee87d3f20b4de7b464bb3f7a04974bf
MarcMBPDev <marc.wenicker@gmail.com>
2017-04-29 16:27:41 -0700

    initial commit for kata https://www.codewars.com/kata/the-sum-of-the-first-and-the-last-prime-factor-make-chains-of-numbers/

---
+++ codewars/src/sum_first_last_pf.py
@@ -0,0 +1,10 @@
+"""Solution to Kata https://www.codewars.com/kata/https://www.codewars.com/kata/the-sum-of-the-first-and-the-last-prime-factor-make-chains-of-numbers/."""
+
+def sflpf_data(val, nMax):
+    """Return a sorted list of numbers whose first and last prime factor sum up to val."""
+    pass
+
+
+def prime_factors(n):
+    """Return a list of prime factors for n."""
+    pass
---
+++ codewars/src/test_sum_first_last_pf.py
@@ -0,0 +1,15 @@
+"""Tests for kata sum_first_last_pf."""
+
+import pytest
+
+TESTS_PF = [
+    (70, [2, 5, 7]),
+    (4, [2, 2]),
+]
+
+
+@pytest.mark.parametrize('n, result', TESTS_PF)
+def test_prime_factors(n, result):
+    """Test that the correct prime factors are returned."""
+    from sum_first_last_pf import prime_factors
+    assert prime_factors(n) == result
