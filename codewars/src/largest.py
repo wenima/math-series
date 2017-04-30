9e2c088417519e60798f01ab37cfa6b98a22d9d4
MarcMBPDev <marc.wenicker@gmail.com>
2017-04-30 11:44:53 -0700

    add tests for edge case and correct input; made prime checking for concise

--- codewars/src/largest.py
+++ codewars/src/largest.py
@@ -2,18 +2,17 @@
 
 def is_prime(n):
     """Return True if n is a prime."""
-    if abs(n) < 2:
+    if (n % 2 == 0 and n > 2) or n == 1:
         return False
-    if n > 2 and n % 2 == 0:
-        return False
-    for x in range(3, int(n**0.5 + 1), 2):
-        if n % x == 0:
-            return False
-    return True
+    return all(n % i for i in range(3, int(n**0.5) + 1, 2))
 
 
 def largest(n):
     """Return the largest prime with n digits."""
+    try:
+        if n < 1 : return False
+    except TypeError:
+        return False
     start = int(''.join(['9' for i in range(n)]))
     for n in range(start, start - (10 ** n - 1) + 1, -2):
         if is_prime(n):
