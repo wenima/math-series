a772484b65600affffc512ad3488ad99822325b2
MarcMBPDev <marc.wenicker@gmail.com>
2017-05-01 16:54:51 -0700

    pass all tests

--- codewars/src/highest_bi_prime.py
+++ codewars/src/highest_bi_prime.py
@@ -3,12 +3,27 @@
 def highest_biPrimefac(p1, p2, end):
     """Return a list with the highest number with prime factors p1 and p2, the
     exponent for the smaller prime and the exponent for the larger prime."""
-    pass
+    given_primes = set([p1, p2])
+    k1 = 0
+    k2 = 0
+    highest = 0
+    for n in range(end, 0, -1):
+        pf = prime_factors(n)
+        if given_primes == set(pf):
+            if pf.count(p1) > k1 and pf.count(p2) > k2:
+                k1 = pf.count(p1)
+                k2 = pf.count(p2)
+                highest = n
+        if (p1 ** k1) * (p2 ** k2) > n:
+            return [highest, k1, k2]
+    return None
 
 
 def prime_factors(n):
     """Return a list with all prime factors of n."""
     factors = []
+    if n < 2:
+        return factors
     p = 2
     while n >= (p * p):
         if n % p:
--- codewars/src/test_highest_bi_prime.py
+++ codewars/src/test_highest_bi_prime.py
@@ -12,10 +12,24 @@ TESTS_PF = [
     (12, [2, 2, 3]),
 ]
 
+TESTS_HIGHEST = [
+    (2, 3, 50, [48, 4, 1]),
+    (5, 11, 1000, [605, 1, 2]),
+    (3, 13, 5000, [4563, 3, 2]),
+    (5, 7, 5000, [4375, 4, 1]),
+]
+
 
 
 @pytest.mark.parametrize('n, result', TESTS_PF)
 def test_prime_factors(n, result):
     """Test that the correct prime factors are returned."""
-    from highest_biPrimefac import prime_factors
+    from highest_bi_prime import prime_factors
     assert prime_factors(n) == result
+
+
+@pytest.mark.parametrize('p1, p2, end, result', TESTS_HIGHEST)
+def test_highest_biPrimefac(p1, p2, end, result):
+    """Test that the correct result are returned."""
+    from highest_bi_prime import highest_biPrimefac
+    assert highest_biPrimefac(p1, p2, end) == result
