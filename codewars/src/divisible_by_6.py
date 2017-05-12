abb78c2fef368eae3636fac531d17866c296e720
MarcMBPDev <marc.wenicker@gmail.com>
2017-05-11 17:28:34 -0700

    add code to deal with special case separately

--- codewars/src/divisible_by_6.py
+++ codewars/src/divisible_by_6.py
@@ -1,17 +1,15 @@
 """This module solves kata https://www.codewars.com/kata/simple-fun-number-258-is-divisible-by-6."""
 
-from math import sqrt
 
 def is_divisible_by_6(s):
     """Return a list of all numbers divisible by 6 given input string and asterisk
     replaced by a digit."""
-    if int(s[-1]) % 2:
-        return []
+    if s == '*': return ['6']
+    out = []
     num = [c for c in s]
     idx = num.index('*')
-    out = []
     for n in range(10):
         num[idx] = str(n)
-        if sum([int(x) for x in num]) % 3 == 0:
-            out.append(''.join(num))
+        if int(num[-1]) % 2 == 0 and sum([int(x) for x in num]) % 3 == 0:
+            out.append(''.join(num) if int(num[0]) else ''.join(num[1:]))
     return out
