da3519a9fed6e98461ccb9940bbd7d97848dcbb8
MarcMBPDev <marc.wenicker@gmail.com>
2017-06-15 09:39:42 -0700

    refactor using generator expression and added better solution from kata's solutions

--- codewars/src/fibfactorials.py
+++ codewars/src/fibfactorials.py
@@ -1,19 +1,27 @@
 """Solution for https://www.codewars.com/kata/fib-factorials/."""
 
-from math import factorial
+from math import factorial as fac
 
 def sum_fib(n):
     """Return the sum of the factorials of the first n fibonacci numbers."""
-    out = 0
-    for x in range(n):
-        out += factorial(fib(x))
-    return out
-    
+    return sum(fac(fib(x)) for x in range(n))
 
 def fib(n):
     """Return the nth fibonacci number."""
     a = 0
     b = 1
-    for __ in range(n):
+    for _ in range(n):
         a, b = b, a + b
     return a
+
+
+"""nicer solution"""
+from math import factorial as fac
+
+def sum_fib_nicer(n):
+    a, b, s = 0, 1, 0
+    while n:
+        s += fac(a)
+        a, b = b, a+b
+        n -= 1
+    return s
